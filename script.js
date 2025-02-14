1c4f44e9dfa485f827cd108e4a45701624953363
Isabela <isresplandi@gmail.com>
2025-02-13 21:29:41 -0300

    update

--- script.js
+++ script.js
@@ -1,38 +1,50 @@
 const pokemonName = document.querySelector('.pokemon_name');
-const pokemonNumber = document.querySelector('number');
+const pokemonNumber = document.querySelector('.number');
 const pokemonImage = document.querySelector('.imgpokemon');
 
 const form = document.querySelector('.form');
 const input = document.querySelector('.input_search');
 
 const buttonPrev = document.querySelector('.btn-prev');
-const buttonNext = document.querySelector('btn-next');
+const buttonNext = document.querySelector('.btn-next');
 
-let searchPokemon = 1;
+//let searchPokemon = 1;
 
 
 const fetchPokemon = async(pokemon) =>{
-    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
+    //no caso de digitar valores invalidos
     if(APIResponse.status === 200){
         const data = await APIResponse.json();
         return data;
     }
-}
+    
+    }
+
 
 const renderPokemon = async (pokemon) =>{
+
+    pokemonName.innerHTML = 'Loading...';
+    pokemonNumber.innerHTML = ''
     const data = await fetchPokemon(pokemon);
 
-    pokemonName.innerHTML = data.name;
-    pokemonNumber.innerHTML = data.id;
-    pokemonImage.src = data ['sprites']['versions']['generation-v']['black-white']['animated']['front-default'];
+        if(data){
+            pokemonName.innerHTML = data.name;
+            pokemonNumber.innerHTML = data.id;
+            pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front-default'];
+            input.value = '';
+        }else{
+            pokemonName.innerHTML = "Not Found";
+            pokemonNumber.innerHTML = '';
+        }
+    
 }
-renderPokemon('20');
-
+//pesquisa no formulario 
  form.addEventListener(
     'submit', (event) =>{
         event.preventDefault();
 
-        renderPokemon(input.value);
+        renderPokemon(input.value.toLowerCase()); //no caso de escrever com a letra Maiuscul
+        
         //console.log(input.value);
     });
